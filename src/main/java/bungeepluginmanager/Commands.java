0a0154cc1a80272f9944576486a6d25a0faee75a
CommissarVoop <viperlv2@inbox.lv>
2020-06-05 19:55:26 +0300

    Command Make Over

--- src/main/java/bungeepluginmanager/Commands.java
+++ src/main/java/bungeepluginmanager/Commands.java
@@ -2,10 +2,15 @@ package bungeepluginmanager;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.plugin.TabExecutor;
 import org.yaml.snakeyaml.Yaml;
 
 import net.md_5.bungee.api.ChatColor;
@@ -16,76 +21,105 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginDescription;
 
-public class Commands extends Command {
+import static net.md_5.bungee.api.ChatColor.*;
+
+public class Commands extends Command implements TabExecutor {
 
 	public Commands() {
 		super("bungeepluginmanager", "bungeepluginmanager.cmds", "bpm");
 	}
-
-	//TODO: split to subcommands
+	@Override
+	public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+		if (sender.hasPermission("bungeepluginmanager.cmds")) {
+			List<String> result = new ArrayList<String>();
+			if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("")) {
+					result.add("help");
+					result.add("list");
+					result.add("load");
+					result.add("reload");
+					result.add("unload");
+					return result;
+				}
+				if (args[0].startsWith("l")) {
+					result.add("list");
+					result.add("load");
+					return result;
+				}
+				if ((args[0].startsWith("h")) || (args[0].endsWith("help"))) {
+					result.add("help");
+					return result;
+				}
+				if ((args[0].startsWith("li")) || (args[0].endsWith("list"))) {
+					result.add("list");
+					return result;
+				}
+				if ((args[0].startsWith("lo")) || (args[0].endsWith("load"))) {
+					result.add("load");
+					return result;
+				}
+				if ((args[0].startsWith("r")) || (args[0].endsWith("reload"))) {
+					result.add("reload");
+					return result;
+				}
+				if ((args[0].startsWith("u")) || (args[0].endsWith("unload"))) {
+					result.add("unload");
+					return result;
+				}
+			}
+			if (args.length == 2) {
+				if ((args[0].equalsIgnoreCase("help")) || (args[0].equalsIgnoreCase("list"))) {
+					return BungeePluginManager.TabshowNone();
+				}
+				if ((args[0].equalsIgnoreCase("reload")) || (args[0].equalsIgnoreCase("unload"))) {
+					List<String> plugin = new ArrayList<String>();
+					for (Plugin pluginlist : ProxyServer.getInstance().getPluginManager().getPlugins()) {
+						String pluglist = pluginlist.getDescription().getName();
+						plugin.add(pluglist);
+					}
+					return plugin;
+				}
+				if (args[0].equalsIgnoreCase("load")) {
+					List<String> jarfile = new ArrayList<String>();
+					File parentFolder = new File (BungeePluginManager.get().getDataFolder().getParent());
+					for (File jar : parentFolder.listFiles()) {
+						if (jar.isFile()) {
+							try {
+								String filename = jar.getName();
+								filename = filename.trim().replaceAll(".jar", "");
+								jarfile.add(filename);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					return jarfile;
+				}
+			}
+			if (args.length >= 3) {
+				return BungeePluginManager.TabshowNone();
+			}
+		} else {
+			return BungeePluginManager.TabshowNone();
+		}
+		return BungeePluginManager.TabshowNone();
+	}
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if (args.length < 1) {
-			sender.sendMessage(textWithColor("Not enough args", ChatColor.RED));
+		if (args.length == 0) {
+			sendHelp(sender);
 			return;
-		}
-		switch (args[0].toLowerCase()) {
-			case "list": {
-				sender.sendMessage(textWithColor(ProxyServer.getInstance().getPluginManager().getPlugins().stream().map(plugin -> plugin.getDescription().getName()).collect(Collectors.joining(", ")), ChatColor.GREEN));
+		} else if (args[0].equalsIgnoreCase("list")) {
+			sender.sendMessage(textWithColor(ProxyServer.getInstance().getPluginManager().getPlugins().stream().map(plugin -> plugin.getDescription().getName()).collect(Collectors.joining(", ")), ChatColor.GREEN));
+			return;
+		} else if (args[0].equalsIgnoreCase("reload")) {
+			if (args.length == 1) {
+				ComponentBuilder builder = new ComponentBuilder("");
+				builder.append("---- BungeePluginManager ----\n").color(GOLD).bold(true);
+				builder.append("/bpm reload ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm reload ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Reloads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+				sender.sendMessage(builder.create());
 				return;
-			}
-			case "unload": {
-				if (args.length < 2) {
-					sender.sendMessage(textWithColor("Not enough args", ChatColor.RED));
-					return;
-				}
-
-				Plugin plugin = findPlugin(args[1]);
-				if (plugin == null) {
-					sender.sendMessage(textWithColor("Plugin not found", ChatColor.RED));
-					return;
-				}
-
-				Exception unloadError = PluginUtils.unloadPlugin(plugin);
-				sender.sendMessage(textWithColor("Plugin unloaded", ChatColor.YELLOW));
-				if (unloadError != null) {
-					sender.sendMessage(textWithColor("Errors occured while disabling plugin, see console for more details", ChatColor.RED));
-					unloadError.printStackTrace();
-				}
-				return;
-			}
-			case "load": {
-				if (args.length < 2) {
-					sender.sendMessage(textWithColor("Not enough args", ChatColor.RED));
-					return;
-				}
-
-				Plugin plugin = findPlugin(args[1]);
-				if (plugin != null) {
-					sender.sendMessage(textWithColor("Plugin is already loaded", ChatColor.RED));
-					return;
-				}
-				File file = findFile(args[1]);
-				if (!file.exists()) {
-					sender.sendMessage(textWithColor("Plugin not found", ChatColor.RED));
-					return;
-				}
-
-				try {
-					PluginUtils.loadPlugin(file);
-					sender.sendMessage(textWithColor("Plugin loaded", ChatColor.YELLOW));
-				} catch (Throwable t) {
-					sender.sendMessage(textWithColor("Error occured while loading plugin, see console for more details", ChatColor.RED));
-					t.printStackTrace();
-				}
-				return;
-			}
-			case "reload": {
-				if (args.length < 2) {
-					sender.sendMessage(textWithColor("Not enough args", ChatColor.RED));
-					return;
-				}
-
+			} else {
 				Plugin plugin = findPlugin(args[1]);
 				if (plugin == null) {
 					sender.sendMessage(textWithColor("Plugin not found", ChatColor.RED));
@@ -107,6 +141,61 @@ public class Commands extends Command {
 				}
 				return;
 			}
+		} else if (args[0].equalsIgnoreCase("load")) {
+			if (args.length == 1) {
+				ComponentBuilder builder = new ComponentBuilder("");
+				builder.append("---- BungeePluginManager ----\n").color(GOLD).bold(true);
+				builder.append("/bpm load ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm load ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Loads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+				sender.sendMessage(builder.create());
+				return;
+			} else {
+				Plugin plugin = findPlugin(args[1]);
+				if (plugin != null) {
+					sender.sendMessage(textWithColor("Plugin is already loaded", ChatColor.RED));
+					return;
+				}
+				File file = findFile(args[1]);
+				if (!file.exists()) {
+					sender.sendMessage(textWithColor("Plugin not found", ChatColor.RED));
+					return;
+				}
+
+				try {
+					PluginUtils.loadPlugin(file);
+					sender.sendMessage(textWithColor("Plugin loaded", ChatColor.YELLOW));
+				} catch (Throwable t) {
+					sender.sendMessage(textWithColor("Error occured while loading plugin, see console for more details", ChatColor.RED));
+					t.printStackTrace();
+				}
+				return;
+			}
+		} else if (args[0].equalsIgnoreCase("unload")) {
+			if (args.length == 1) {
+				ComponentBuilder builder = new ComponentBuilder("");
+				builder.append("---- BungeePluginManager ----\n").color(GOLD).bold(true);
+				builder.append("/bpm unload ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm unload ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Unloads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+				sender.sendMessage(builder.create());
+				return;
+			} else {
+				Plugin plugin = findPlugin(args[1]);
+				if (plugin == null) {
+					sender.sendMessage(textWithColor("Plugin not found", ChatColor.RED));
+					return;
+				}
+				Exception unloadError = PluginUtils.unloadPlugin(plugin);
+				sender.sendMessage(textWithColor(GRAY + ITALIC.toString() + args[1] + " unloaded", ChatColor.YELLOW));
+				if (unloadError != null) {
+					sender.sendMessage(textWithColor("Errors occured while disabling plugin, see console for more details", ChatColor.RED));
+					unloadError.printStackTrace();
+				}
+				return;
+			}
+		} else if (args[0].equalsIgnoreCase("help")) {
+			sendHelp(sender);
+			return;
+		} else {
+			sender.sendMessage(new TextComponent(RED + "Error: " + DARK_RED + "Sub-Command Not Found!"));
+			return;
 		}
 	}
 
@@ -142,11 +231,19 @@ public class Commands extends Command {
 		}
 		return new File(folder, pluginname+".jar");
 	}
-
 	private static TextComponent textWithColor(String message, ChatColor color) {
 		TextComponent text = new TextComponent(message);
 		text.setColor(color);
 		return text;
 	}
-
+	private static void sendHelp(CommandSender sender) {
+		ComponentBuilder builder = new ComponentBuilder("");
+		builder.append("---- BungeePluginManager ----\n").color(GOLD).bold(true);
+		builder.append("/bpm help: ").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/bpm help")).color(GOLD).bold(false).append("Display this message\n", ComponentBuilder.FormatRetention.NONE);
+		builder.append("/bpm load ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm load ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Loads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+		builder.append("/bpm unload ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm unload ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Unloads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+		builder.append("/bpm reload ").event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/bpm reload ")).color(GOLD).append("<plugin>: ").color(GREEN).append("Reloads a plugin\n", ComponentBuilder.FormatRetention.NONE);
+		builder.append("/bpm list: ").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/bpm list")).color(GOLD).append("List all plugins on the bungee", ComponentBuilder.FormatRetention.NONE);
+		sender.sendMessage(builder.create());
+	}
 }
